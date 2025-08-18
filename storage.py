195ecc95a16e441675985d7de47f9462a378ca5f
salatovaksenia5-afk <salatovaksenia5@gmail.com>
2025-08-19 04:02:52 +0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,8 +1,6 @@
 import json
 from pathlib import Path
-
 FILE_PATH = Path("storage.json")
-
 # Загружаем или создаём файл
 if FILE_PATH.exists():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
@@ -15,32 +13,25 @@ else:
     with open(FILE_PATH, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
      facts = {}  # словарь вида: {subject: [факт1, факт2, ...]}
-
 def add_fact(subject: str, fact: str):
     if subject not in facts:
         facts[subject] = []
     facts[subject].append(fact)
-
 def list_facts(subject: str):
     return facts.get(subject, [])
        
-
 # ---------- Комплименты ----------
 def add_compliment(text: str):
     data["compliments"].append(text)
     _save()
-
 def get_compliments():
     return data["compliments"].copy()
-
 # ---------- Реакции ----------
 def set_reactions(reactions: list[str]):
     data["reactions"] = reactions.copy()
     _save()
-
 def get_reactions():
     return data["reactions"].copy()
-
 # ---------- Сохранение ----------
 def _save():
     with open(FILE_PATH, "w", encoding="utf-8") as f:
